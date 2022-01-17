8b959402dbd22f4268678c6c1c57bffa3a2a7a91
Kashif Abbasi <kashif.abbasi@deptagency.com>
2022-01-17 22:58:53 +0100

    RESTful implementation with GET, POST, PUT

--- data/products.go
+++ data/products.go
@@ -1,21 +1,79 @@
 package data
 
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"time"
+)
 
 type Product struct {
-	ID          int
-	Name        string
-	Description string
-	SKU         string
-	CreatedOn   string
-	UpdatedOn   string
-	DeletedOn   string
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	SKU         string `json:"sku"`
+	CreatedOn   string `json:"_"`
+	UpdatedOn   string `json:"_"`
+	DeletedOn   string `json:"_"`
 }
 
-func getProducts() []*Product {
+//Collection of Proucts
+type Products []*Product
+
+//Encode ToJSON
+func (p *Products) ToJSON(w io.Writer) error {
+	encoder := json.NewEncoder(w)
+	return encoder.Encode(p)
+}
+
+//Decode FromJSON
+func (p *Product) FromJSON(r io.Reader) error {
+	decoder := json.NewDecoder(r)
+	return decoder.Decode(p)
+}
+
+//Get all products
+func GetProducts() Products {
 	return productList
 }
 
+//Add product
+func AddProduct(p *Product) {
+	p.ID = getNextID()
+	productList = append(productList, p)
+}
+
+//get product next ID
+func getNextID() int {
+	lp := productList[len(productList)-1]
+	return lp.ID + 1
+}
+
+//Update product
+func UpdateProduct(id int, p *Product) error {
+	_, pos, err := findProductById(id)
+	if err != nil {
+		return err
+	}
+
+	p.ID = id
+	productList[pos] = p
+
+	return nil
+}
+
+func findProductById(id int) (*Product, int, error) {
+	for i, p := range productList {
+		if p.ID == id {
+			return p, i, nil
+		}
+	}
+	return nil, -1, ErrProductNotFound
+}
+
+var ErrProductNotFound = fmt.Errorf("Product not Found!")
+
+//Temporary product storage
 var productList = []*Product{
 	&Product{
 		ID:          1,
--- handlers/products.go
+++ handlers/products.go
@@ -1,34 +1,101 @@
 package handlers
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
+
+	"github.com/gr8abbasi/go-microservice/data"
 )
 
 type Products struct {
 	l *log.Logger
 }
 
+//Constructor
 func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
+//Server HTTP requests by HTTP verbs
 func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
-
 		p.getProducts(rw, r)
+		return
 	}
+
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
+
+	if r.Method == http.MethodPut {
+		regex := regexp.MustCompile("/([0-9]*)")
+		g := regex.FindAllStringSubmatch(r.URL.Path, -1)
+
+		if len(g) != 1 {
+			p.l.Println("Invalid URI, more than one id")
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		if len(g[0]) != 2 {
+			p.l.Println("Invalid URI, more than one capture group")
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		stringId := g[0][1]
+		id, err := strconv.Atoi(stringId)
+
+		if err != nil {
+			p.l.Println("Unable to convert string to int %s", stringId)
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		p.updateProduct(id, rw, r)
+		return
+	}
+
+	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
+//GET request to get all products
 func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
 	p.l.Println("Handle Get Products")
 
-	lp := data.getProducts()
+	lp := data.GetProducts()
 
-	encoder := json.NewEncoder(rw)
-	err := encoder.Encode(lp)
+	err := lp.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Unable to marshal JSON", http.StatusInternalServerError)
 	}
+}
+
+//POST request to add product
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST Product")
+
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal JSON", http.StatusBadRequest)
+	}
+
+	data.AddProduct(prod)
+}
+
+//PUT request to update product
+func (p *Products) updateProduct(id int, rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle PUT Product")
+
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal JSON", http.StatusBadRequest)
+	}
+
+	data.UpdateProduct(id, prod)
 }
